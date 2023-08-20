427e81d0720b4f2839822096fa3883a9608b5b3e
Emon <emon22205101732@diu.edu.bd>
2023-08-20 21:17:06 +0600

    update

--- array.php
+++ array.php
@@ -11,12 +11,23 @@ $student =array(
      echo $student[$i],"\n";
  }
  var_dump($student);
-echo ($name);
+//echo ($name);
 
 // if it is in string format then it will be like this.....
  $name1= explode(', ', 'emon, limon, shaib');
  $name1String=join(', ',$name1);
 
 
- echo $name1String; 
+ //echo $name1String; 
 // echo count($name1);
+
+
+
+
+$sampel1=[
+    [1,2,3,4,5],
+    [11,22,33,44,555],
+    [111,122,33,44,5,[8,7,6,5]]
+];
+//print_r($sampel1);
+echo $sampel1[2][3];
