00e9fc8b8a127cb824677aeae7538c97b42ebe10
Emon <emon22205101732@diu.edu.bd>
2023-10-07 12:42:39 +0600

    update

---
+++ oop.php
@@ -0,0 +1,36 @@
+<?php
+class Human{
+
+function __construct()
+{
+    echo "Emon is Good";
+}
+
+
+
+    public  $name;
+    function sayHi(){
+        echo "Hello\n";
+    }
+}
+class Cat{
+    function sayHi(){
+        echo "\nMeow\n";
+    }
+}
+class Dog{
+    function sayHi(){
+        echo "new new";
+    }
+}
+
+$h1= new Human();
+$h1->name ="Emon"; //set
+$c2= new Cat();
+$u3 =new Dog();
+
+$h1->sayHi();
+echo $h1->name; //get
+$c2->sayHi();
+$u3->sayHi();
+?>
