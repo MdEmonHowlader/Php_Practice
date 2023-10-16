552d49c51ca583baea410e5c51e72e6beee7abcc
Emon <emon22205101732@diu.edu.bd>
2023-10-16 15:06:32 +0600

    Parent to Child

---
+++ prentToChildClass.php
@@ -0,0 +1,19 @@
+<?php 
+class PrentClass{
+    protected $name;
+    function __construct($name) {
+        $this->name= $name;
+        $this->emon();
+    
+    }
+    function emon(){
+        echo "NO entry Emon. {$this->name}\n";
+    }
+}
+class ChiildClass extends PrentClass{
+    function emon(){
+        parent:: emon();
+        echo "Hi Emon\n";
+    }
+}
+$eee= new ChiildClass("He is bad Student.... ");
