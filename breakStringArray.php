9d5b9ff1394348e191d2196d3f8cce3ca0d866f6
Emon <emon22205101732@diu.edu.bd>
2023-11-28 00:50:08 +0600

    breaks a string

---
+++ breakStringArray.php
@@ -0,0 +1,12 @@
+<?php
+$city=explode(', ','Barishal, Dhaka, Chattogram, Khulna, Rajshahi,');
+echo $city[2];
+
+
+
+//The explode() function breaks a string into an array.
+
+//Note: The "separator" parameter cannot be an empty string.
+
+//Note: This function is binary-safe.
+?>
--- testcode.php
+++ testcode.php
@@ -1,7 +1,5 @@
 <?php
-
 $city=explode(', ','Barishal, Dhaka, Chattogram, Khulna, Rajshahi,');
-
 echo $city[2];
 
-
+?>
