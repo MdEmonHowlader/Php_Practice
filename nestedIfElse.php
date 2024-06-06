2ec3beb28b12e72a9c06d4dd351d13f0ea1461e4
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-06-06 13:16:11 +0600

    Nested if else in php

---
+++ nestedIfElse.php
@@ -0,0 +1,12 @@
+<?php
+$ag=18;
+if($ag>=18){
+    echo "Yes you can voat";
+}elseif($ag==17){
+    echo "Please wait one more years";
+}elseif($ag==16){
+    echo "Please wait Two more years";
+}else{
+    echo "Please Try later";
+}
+?>
