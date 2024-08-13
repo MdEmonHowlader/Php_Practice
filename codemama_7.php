35f89062e83831d99730cf677d03fbffb7273a60
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-08-13 14:35:31 +0600

    Convert to Hour /Minutes

--- codemama_7.php
+++ codemama_7.php
@@ -1,12 +1,18 @@
 <?php
-    function convertToHourMinute($seconds){
-        $hours = floor($seconds / 3600);
-        $minutes= floor($seconds % 3600)/60;
+    // function convertToHourMinute($seconds){
+    //     $hours = floor($seconds / 3600);
+    //     $minutes= floor($seconds % 3600)/60;
 
-        return $hours . ':' . $minutes;
-    }
+    //     return $hours . ':' . $minutes;
+    // }
 
-    $seconds = intval(trim(fgets(STDIN)));
-    echo convertToHourMinute($seconds);
+    // $seconds = intval(trim(fgets(STDIN)));
+    // echo convertToHourMinute($seconds);
+
+    $seconds =(int)trim(fgets(STDIN));
+    $hours=$seconds/3600 |0;
+    $minutes=($seconds%3600)/60 |0;
+
+    echo $hours. ":" . $minutes ."\n";
 
 ?>
