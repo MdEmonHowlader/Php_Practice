24aa4e4e619c724a3a5bb604e5bde0f69250cd32
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-09-07 19:39:37 +0600

    the maximum of three numbers

--- codemama_11.php
+++ codemama_11.php
@@ -1,7 +1,9 @@
 <?php
-$num1=(int)fgets( STDIN);
-$num2=(int)fgets( STDIN);
-$num3=(int)fgets( STDIN);
+// $num1=(int)fgets( STDIN);
+// $num2=(int)fgets( STDIN);
+// $num3=(int)fgets( STDIN);
+
+fscanf(STDIN , "%d %d %d", $num1, $num2, $num3 );
 $max=max($num1,$num2,$num3);
 echo $max;
 
