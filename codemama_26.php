79839d924ad3ef6d5af3fa66e62d08151625b788
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-11-23 23:45:02 +0600

    leap year

--- codemama_26.php
+++ codemama_26.php
@@ -1,14 +1,11 @@
 <?php 
-$input = trim(fgets(STDIN));
-if(is_numeric($input) && (int)$input>0){
-    $year = (int)$input;
-if(($year % 4 == 0 && $year % 100 != 0 )|| $year % 400 == 0){
-    echo $year . " is a leap year"; 
-}else{
-    echo $year . " is not a leap year";
+$year = (int)trim(fgets(STDIN));
+if (($year % 4 == 0 && $year % 100 != 0) || $year % 400 == 0) {
+    echo "$year is a leap year.\n";
+} else {
+    echo "$year is not a leap year.\n";
 }
 
-}
 
 
 
